1768b497005f5f9a9bb5149933f4428fd85b171f
work <temogogitidze@redberry.ge>
2023-07-03 22:53:40 +0400

    refactor: add separate widgets, prepare code for multiple screen routing

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -12,6 +12,7 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             'assets/images/quiz-logo.png',
             width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
           ),
           const SizedBox(
             height: 80,
@@ -23,12 +24,13 @@ class StartScreen extends StatelessWidget {
           const SizedBox(
             height: 30,
           ),
-          OutlinedButton(
+          OutlinedButton.icon(
               onPressed: () {},
               style: OutlinedButton.styleFrom(
                 foregroundColor: Colors.white,
               ),
-              child: const Text('Start Quiz!'))
+              icon: const Icon(Icons.arrow_right),
+              label: const Text('Start Quiz!'))
         ],
       ),
     );
