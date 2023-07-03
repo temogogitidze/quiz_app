b6620b8a21aec503f70a0f9dc1afeb48e3aaaf50
work <temogogitidze@redberry.ge>
2023-07-03 21:53:03 +0400

    feat: add icon, button and text for start screen widget

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -2,9 +2,35 @@ import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
-  
+
   @override
   Widget build(context) {
-    return const Text('Start screen');
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'The best quiz ull ever see',
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+              ),
+              child: const Text('Start Quiz!'))
+        ],
+      ),
+    );
   }
 }
