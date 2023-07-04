6fb8de13fa99cffb236c09906c6769b328b5d252
work <temogogitidze@redberry.ge>
2023-07-04 13:07:11 +0400

    refactor: extract answer buttons into separate reusable component

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -17,12 +18,14 @@ class _QuestionsScreen extends State<QuestionsScreen> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('question ...'),
-          const SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Answer 1'),
+          const Text(
+            'question ...',
+            style: TextStyle(color: Colors.white),
           ),
+          const SizedBox(height: 30),
+          AnswerButton('Answer 1', () {}),
+          AnswerButton('Answer 2', () {}),
+          AnswerButton('Answer 3', () {})
         ],
       ),
     );
