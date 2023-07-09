dde74423a22ded1d0eef34011faabef73c7a1291
work <temogogitidze@redberry.ge>
2023-07-09 21:20:14 +0400

    feat: pass dynamic data for results page

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -29,7 +29,7 @@ class _QuizState extends State<Quiz> {
     if (selectedAnswers.length == questions.length) {
       setState(() {
         selectedAnswers = [];
-        activeScreen = const ResultsScreen();
+        activeScreen = ResultsScreen(chosenAnswers: selectedAnswers);
       });
     }
   }
